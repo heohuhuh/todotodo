9079684fef0bdb67ed31962d30220d0eeacd5986
DESKTOP-SU1D8SB\wlsdn <wlsdnr3636@gmail.com>
2023-01-10 14:20:01 +0900

    todoList 23.01.10. 0.0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import List from './List';
+import { render } from "@testing-library/react";
 
 function App() {
+  let [할일,할일변경] = useState()
+  let [목록,목록변경] = useState([])
+  const onChange = (e) => {
+    할일변경(e.target.value)
+  };
+  const 목록생성 = () =>{
+    let listTodo = [...목록,할일];
+    목록변경(listTodo)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      <p>일단 todo</p>
+      <input className="input" 
+      onChange={ onChange }
+      onKeyDown={(e)=>{
+        console.log(e)
+        if(e.key === "Enter"){
+          목록생성()
+        }
+      }}
+      placeholder = "입력칸" value={ 할일 }></input>
+      <button onClick={
+          목록생성
+      }>
+        추가
+      </button>
+    <List test = { 목록 }/>
     </div>
   );
 }
